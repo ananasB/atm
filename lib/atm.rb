2c2a98c441f80cf42848ddaba10b95715e1fc0f6
Anna Bodnarchuk <vchytel319@gmail.com>
2018-08-10 13:05:57 +0300

    Add instructions' commands to atm

--- lib/atm.rb
+++ lib/atm.rb
@@ -14,8 +14,35 @@ class Atm
     
     if @accounts[@account_number]['password'] == @account_pass
       puts "Hello, #{@accounts[@account_number]['name']}!"
+      print_commands
     else
       puts "ERROR: ACCOUNT NUMBER AND PASSWORD DON'T MATCH"
     end
+  end
+
+  def print_commands
+    commands = [
+      [1, 'Display Balance'], [2, 'Withdraw']
+    ]
+    puts 'Please Choose From the Following Options:'
+    commands.each { |key, value| puts "#{key}.  #{value}." }
+
+    user_action = gets.chomp.to_i
+    process_command(user_action)
+  end
+
+  def process_command(user_action)
+    case
+      when 1
+        account_balance
+      when 2
+        withdraw
+    end
+  end
+
+  def account_balance
+  end
+
+  def withdraw
   end
 end
